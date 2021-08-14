67e7c675e8f66f377002f09caa896ee5dbe8c36f
Alexander-Aparicio <73755700+Alexander-Aparicio@users.noreply.github.com>
2021-08-13 21:09:37 -0500

    renderizado de preguntas y suma de puntaje

--- js/ComponentOne.js
+++ js/ComponentOne.js
@@ -3,6 +3,9 @@ const ComponentOne = ()=>{
     const $theme = d.getElementById('theme'),
         $question = d.getElementById('question'),
         $options = d.getElementById('options'),
+        $op1 = d.getElementById('op1'),
+        $op2 = d.getElementById('op2'),
+        $op3 = d.getElementById('op3'),
         $time = d.getElementById('time'),
         $recordPlayer = d.getElementById('recordComponentOne'),
         $score = d.getElementById('score'),
@@ -14,21 +17,27 @@ const ComponentOne = ()=>{
         $player = d.getElementById('player'),
         $email = d.getElementById('email'),
         $playerName = d.getElementById('playerName'),
-        $option1 = d.getElementById('op1'),
-        $option2 = d.getElementById('op2'),
-        $option3 = d.getElementById('op3'),
+        $option1 = d.getElementById('liop1'),
+        $option2 = d.getElementById('liop2'),
+        $option3 = d.getElementById('liop3'),
         fragment = d.createDocumentFragment()
-        
-    let loaderState = 'Inactive'
+         
+    let loaderState = 'Full Load'
     let randomNumberArray = []
     let questions = []
     let options = []
+    let points = 0
     const player = []
     let i = 0
 
     const activeLoader = ()=>{
         $loader.classList.remove('none')
         $loader.classList.add('inlineBlock')
+        $option1.classList.add('none')
+        $option2.classList.add('none')
+        $option3.classList.add('none')
+        $question.classList.add('none')
+
         loaderState = 'Loading...'
     }
     const inactiveLoader = ()=>{
@@ -140,7 +149,88 @@ const ComponentOne = ()=>{
             });
         }
     } 
-    
+    const enableContainers = (answer1,answer2,answer3,question)=>{
+        if(answer1.classList.contains('none')){answer1.classList.remove('none')}
+        if(answer2.classList.contains('none')){answer2.classList.remove('none')}
+        if(answer3.classList.contains('none')){answer3.classList.remove('none')}
+        if(question.classList.contains('none')){question.classList.remove('none')}
+    }
+    const renderPlayer = ()=>{
+        $playerName.textContent = $player.value
+    }
+    const evaluation = (objectOptions,idOption,arrayRandom,i)=>{
+        if(i< arrayRandom.length){
+            const idOpciones = arrayRandom[i]
+            console.log(`Estoy dentro de evaluation ${idOpciones}`)
+            function filtration (el){return parseInt(el.id)===idOpciones}
+            const objectOpciones = objectOptions.find(filtration)
+            const opcionesDeRespuesta = objectOpciones.opciones
+            console.log(opcionesDeRespuesta)
+            console.log(idOption)
+            function filtro (el){return el.id=== `${idOption}`}
+            const opcionEvaluar = opcionesDeRespuesta.find(filtro)
+            console.log(`Estoy dentro de evaluation ${opcionEvaluar} , ${opcionEvaluar.rpta}`)
+            const functionEvaluation = ()=>{
+                if(opcionEvaluar.rpta === 'true'){
+                    d.getElementById(`res${idOption}`).textContent = "✔️"
+                    points=points+10
+                    $score.textContent = points
+                }
+                if(opcionEvaluar.rpta === 'false'){
+                    d.getElementById(`res${idOption}`).textContent = "✖️"
+                }
+            }
+            functionEvaluation()
+        }
+    }
+    const validationRecord = (inputName,inputEmail,record)=>{
+        return new Promise((resolve,reject)=>{
+
+            setTimeout(()=>{
+                if(inputName.value && inputEmail.value){
+                    resolve(record.classList.add('none'))
+                }else{reject('Resgistrate para poder Jugar')}
+            },100)
+        })
+    }
+    const disableOptions = (option1,option2)=>{
+        option1.classList.add('none')
+        option2.classList.add('none')
+    }
+    const cleaningContent = (element)=>{
+        element.textContent = ""
+    }
+    const hideElement = (element)=>{
+        element.classList.add('none')
+    }
+    const gameOver = ()=>{
+        $question.textContent = "¡Terminaste! 🎉"
+        while($options.hasChildNodes()){
+            $options.removeChild($options.firstChild)
+        }
+    }
+    const next = (idInputClick)=>{
+        setTimeout(() => {
+            if(i<randomNumberArray.length){
+                i = i+1
+                d.getElementById(idInputClick).textContent=""
+                activeLoader()
+                setTimeout(() => {
+                    inactiveLoader()
+                    enableContainers($option1,$option2,$option3,$question)
+                    renderQuestions(randomNumberArray,questions,i)
+                    renderAnswer(randomNumberArray,options,i)
+                }, 500)
+            }
+            if(i>=randomNumberArray.length){
+                d.getElementById(idInputClick).textContent=""
+                activeLoader()
+                gameOver()
+                inactiveLoader()
+            }
+
+        }, 1000);
+    }
     // Primer event
     d.addEventListener('click', async (e)=>{
         e.preventDefault()
@@ -149,49 +239,39 @@ const ComponentOne = ()=>{
             $btnOver.classList.remove('none')
             $btnPlay.classList.add('none')
             $recordPlayer.classList.remove('none')
-            $record.classList.toggle('transition')
-        }
-
-        if(e.target === $btnOver){
-
-            $btnPlay.classList.remove('none')
-            $btnOver.classList.add('none')
-            $record.classList.toggle('transition')
-
+            $record.classList.add('transition')
         }
 
         if(e.target === $btnRecord){
 
-            if($player.value && $email.value){
-
-                activeLoader()
-                $recordPlayer.classList.add('none')
-                console.log(`Despues del event submit el valor del estado respuesta es: ${loaderState}`)
-                await getQuestions("http://127.0.0.1:5500/json/questions.json",questions)
-                await getOptions("http://127.0.0.1:5500/json/answers.json",options)
-                await arrayRandomPromise(randomNumberArray,5,4)
-                inactiveLoader()
-                if(loaderState === 'Full Load'){
-                    console.log(`El valor del estado de la respuesta final es: ${loaderState}`)
-                    loaderState = 'Inactive' 
-                    renderQuestions(randomNumberArray,questions,i)
-                    renderAnswer(randomNumberArray,options,i)
-                    $playerName.textContent = $player.value
-                }
-            
-            }else{
-
-            }
-
-        }
-        
-        if(e.target === $option1 || e.target === $option2 || e.target === $option3){
-            i=i+1
+            await validationRecord($player,$email,$recordPlayer)
+            activeLoader()
+            await getQuestions("http://127.0.0.1:5500/json/questions.json",questions)
+            await getOptions("http://127.0.0.1:5500/json/answers.json",options)
+            await arrayRandomPromise(randomNumberArray,5,4)
+            inactiveLoader()
+            enableContainers($option1,$option2,$option3,$question)
             renderQuestions(randomNumberArray,questions,i)
             renderAnswer(randomNumberArray,options,i)
+            renderPlayer()
+            loaderState = 'Load'
+
         }
-
-
+        if(e.target === $option1||e.target === $op1){
+            disableOptions($option2,$option3)
+            evaluation(options,"op1",randomNumberArray,i)
+            next('resop1')
+        }
+        if(e.target === $option2||e.target === $op2){
+            disableOptions($option1,$option3)
+            evaluation(options,"op2",randomNumberArray,i)
+            next('resop2')
+        }
+        if(e.target === $option3||e.target === $op3){ 
+            disableOptions($option2,$option1)
+            evaluation(options,"op3",randomNumberArray,i)
+            next('resop3')
+        }
         if(e.target ===$btnOver){
             location.reload()
         }
